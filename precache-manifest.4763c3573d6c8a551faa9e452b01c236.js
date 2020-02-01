b450603394a8048dedc34181deb93d19a821a43f
Andrii <farikpite@gmail.com>
2020-02-02 00:45:47 +0200

    Updates

---
+++ precache-manifest.4763c3573d6c8a551faa9e452b01c236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc5e57b3c0115e983e73cd57ee946f6",
+    "url": "/Andrii-Pavlenko/react_redux-list-of-posts/build/index.html"
+  },
+  {
+    "revision": "04180cec2b33a141a856",
+    "url": "/Andrii-Pavlenko/react_redux-list-of-posts/build/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "624b983f4d74aa7199dd",
+    "url": "/Andrii-Pavlenko/react_redux-list-of-posts/build/static/js/2.4ca5d0ff.chunk.js"
+  },
+  {
+    "revision": "04180cec2b33a141a856",
+    "url": "/Andrii-Pavlenko/react_redux-list-of-posts/build/static/js/main.2d779fa1.chunk.js"
+  },
+  {
+    "revision": "239fa01d038dd5008565",
+    "url": "/Andrii-Pavlenko/react_redux-list-of-posts/build/static/js/runtime~main.d1d2cc8c.js"
+  }
+]);
